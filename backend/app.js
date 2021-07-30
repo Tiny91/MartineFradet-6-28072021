da03f690edbc35374d175bee53bc4227d3d45707
Martine <tinyfradet@gmail.com>
2021-07-30 15:10:50 +0200

    first test frontend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const Sauce = require('./model/sauce');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+
+const userRoutes = require ('./routes/user');
 
 mongoose.connect('mongodb+srv://Tiny:coursNode@cluster0.1vvuv.mongodb.net/projet0?retryWrites=true&w=majority',
 { useNewUrlParser: true,
@@ -24,11 +28,28 @@ app.use((req, res, next) => {
   app.use(express.urlencoded({extended:true}));
   app.use(express.json());
 
-//ttes les sauces
-app.get('/api/sauces',(req, res, next)=>{
-    Sauce.find()
-    .then(products => res.status(200).json({products}))
-    .catch(error => res.status(400).json({ error }));
-});
+  // app.use('/api/sauces', (req,res,next) =>{
+    // const sauce = [
+      //  {
+          // _id: 'eroimfgjlfh', 
+          // name: 'Blair\'s Ultra Death Sauce',
+          // manufacturer: 'Blair\'s',
+          // // // // description: 'Blair\'s Ultra Death has established itself as a bit of a legend within the hot sauce world. one thing that creator Blair Lazar does well it\'s retaining the flavour in his super-hot sauces. They\'ll melt your face off for sure, but despite the extract they still taste damned fine.Just to emphasise the seriousness of the heat we\'re dealing with here, all Blair\'s super-hot sauces in the Death range now come in a nifty coffin box with his trademark  skull keyring attached to the bottle.',
+          // heat: 9,
+          // likes: 100,
+          // dislikes: 0,
+          // imageUrl: 'https://www.chilliworld.com/content/images/thumbs/0000827_blairs-ultra-death-sauce-in-a-coffin_550.jpeg',
+          // mainPepper: 'Carolina Reaper',
+          // usersLiked: [],
+          // usersDisliked: []
+        // }
+          // ];
+    // res.status(200).json(sauce)
+  // });
+
+// chemin middleware sauces et utilisateurs et images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces',sauceRoutes);
+app.use('/api/auth',userRoutes);
 
 module.exports = app;
